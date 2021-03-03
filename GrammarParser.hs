d0eff32d41348a378b02c9c22c168f74cead819b
Thuger <oleg.ignatenko12@gmail.com>
2021-03-03 11:16:53 +0300

    Ready to parse grammar into Haskell structure

--- GrammarParser.hs
+++ GrammarParser.hs
@@ -7,16 +7,23 @@ import Text.Parsec ((<?>))
 import Control.Applicative
 
 
-parseGrammar :: String -> [Rule]
-parseGrammar grammarText = map grammarFromRule $ map (Parsec.parse parseRule "(source)") grammarLine
+parseGrammar :: String -> Grammar
+parseGrammar grammarText = map grammarFromRule 
+                           $ map (Parsec.parse parseRule "(source)") 
+                           $ filterGrammar grammarLine
   where
     grammarLine = lines grammarText
-
+    -- To remove empty strings and comments
+    filterGrammar :: [String] -> [String]
+    filterGrammar = filter filterComments . filter filterEmpty
+      where 
+        filterEmpty = \x -> x /= ""
+        filterComments = \x -> head x /= '#' 
     grammarFromRule :: Either Parsec.ParseError Rule -> Rule
     grammarFromRule parseRule = 
         case parseRule of
           Right rule -> rule
-
+          Left err -> error $ show err
     parseRule :: Parsec.Parsec String () Rule
     parseRule = do
         prod <- Parsec.many Parsec.letter
@@ -26,34 +33,32 @@ parseGrammar grammarText = map grammarFromRule $ map (Parsec.parse parseRule "(s
       where
         separator = Parsec.spaces >> Parsec.string "::" >> Parsec.spaces
 
-        parseRhs :: Parsec.Parsec String () [[Symbol]]
-        parseRhs =  Parsec.sepBy parseRhsPiece rhsSep
-          where 
-            rhsSep = Parsec.spaces >> Parsec.char '|' >> Parsec.spaces
+    parseRhs :: Parsec.Parsec String () [[Symbol]]
+    parseRhs =  Parsec.sepBy parseRhsPiece rhsSep
+      where 
+        rhsSep = Parsec.spaces >> Parsec.char '|' >> Parsec.spaces
 
-            -- Rhs piece is part of production between |
-            -- Terms and NonTerms could be here
-            -- Terms stays in quote 'term'
-            parseRhsPiece :: Parsec.Parsec String () [Symbol]
-            parseRhsPiece = Parsec.many $ do
-                symbol <- (parseNTerm <|> parseTerm) 
-                Parsec.spaces
-                return symbol
+        -- Rhs piece is part of production between |
+        -- Terms and NonTerms could be here
+        -- Terms stays in quote 'term'
+        parseRhsPiece :: Parsec.Parsec String () [Symbol]
+        parseRhsPiece = Parsec.many $ do
+            symbol <- (parseNTerm <|> parseTerm) 
+            Parsec.spaces
+            return symbol
+          where
+            parseNTerm :: Parsec.Parsec String () Symbol
+            parseNTerm = do
+                nterm <- Parsec.many1 Parsec.letter <?> "Error parsing non terminal"
+                return (Right $ NTerm nterm)
+            parseTerm :: Parsec.Parsec String () Symbol
+            parseTerm = do
+                Parsec.string "'" <?> "First quote is empty"
+                term <- parseWord <|> parseSign <?> "Strange sign"
+                Parsec.string "'"
+                case term of
+                    "E" -> return (Left Epsilon)
+                    _ -> return (Left $ Term term)
               where
-
-                parseNTerm :: Parsec.Parsec String () Symbol
-                parseNTerm = do
-                    nterm <- Parsec.many1 Parsec.letter
-                    return (Right $ NTerm nterm)
-
-                parseTerm :: Parsec.Parsec String () Symbol
-                parseTerm = do
-                    Parsec.string "'"
-                    term <- parseWord <|> parseSign
-                    Parsec.string "'"
-                    case term of
-                        "E" -> return (Left Epsilon)
-                        _ -> return (Left $ Term term)
-                  where
-                    parseSign = Parsec.many1 $ Parsec.oneOf "%&*{},;/[]()"
-                    parseWord = Parsec.many1 Parsec.letter
+                parseSign = Parsec.many1 $ Parsec.oneOf ":%&*{},;/[]()"
+                parseWord = Parsec.many1 Parsec.letter
