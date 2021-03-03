ae6efcccd7c15122966c3db39467b2fa446496ea
Thuger <oleg.ignatenko12@gmail.com>
2021-03-03 12:52:08 +0300

    Write utils for computin FirstFollow sets

--- FirstFollow.hs
+++ FirstFollow.hs
@@ -1,74 +1,87 @@
+module FirstFollow where
+
 import ParserTypes
 
-import Data.Map as Map (Map, member, lookup, insertWith)
-import Data.Set as Set (Set, fromList)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 
-data FirstSetMap = FirstSetMap (Data.Map.Map NonTerminal (Set Terminal)) deriving (Show, Read)
-
-grammar = Grammar [Rule Start "program" [[Terminal "funcDecl", Terminal "varDecl"], [Epsilon]],
-                   Rule NTerm "funcDecl" [[NTerm "block", Terminal "+"]]]
+type FirstSet = Set.Set Terminal
+type FirstSetMap = Map.Map NonTerminal FirstSet
 
 --
 -- Utilities
 --
-getFirstSet :: NonTerminal -> FirstSetMap -> Set.Set Terminal
-getFirstSet nonTerminal firstMap = f $ Map.lookup nonTerminal firstMap 
-  where 
-    f Nothing = Set.Set.empty
-    f (Just set) = set
+--getFirstSet :: NonTerminal -> FirstSetMap -> Set.Set Terminal
+--getFirstSet nonTerminal firstMap = f $ Map.lookup nonTerminal firstMap 
+  --where 
+    --f Nothing = Set.empty
+    --f (Just set) = set
 
-mergeIntoFirstSet :: FirstSetMap -> NonTerminal -> FirstSet -> FirstSetMap
-mergeFirstSet map nonTerm newSet = Map.insertWith Set.union nonTerm newSet map
+--mergeIntoFirstSet :: FirstSetMap -> NonTerminal -> FirstSet -> FirstSetMap
+--mergeIntoFirstSet map nonTerm newSet = Map.insertWith Set.union nonTerm newSet map
 
-getNonTerminals :: Grammar -> Set NonTerminal
+getNonTerminals :: Grammar -> Set.Set NonTerminal 
+getNonTerminals (rule:remain) = Set.union (Set.singleton (lhs rule)) (getNonTerminals remain)
+getNonTerminals [] = Set.empty
 
+insertIntoFirstSet :: FirstSetMap -> NonTerminal -> Terminal -> FirstSetMap
+insertIntoFirstSet firstSet nterm term = Map.insertWith Set.union nterm (Set.singleton term) firstSet
 -- 
 -- Create first set for current grammar
 --
-firstSet :: Grammar -> FirstSetMap
-firstSet (grammarRule:remain) = first grammar (firstInit grammar)
-  where
+--firstSet :: Grammar -> FirstSetMap
+--firstSet (rule:remain) = first grammar (firstInit grammar)
+--  where
     -- Init empty sets for all non terminals
-    firstInit :: Grammar -> FirstSetMap
-    firstInit grammar = FirstSetMap $ 
-        Map.fromList $ fmap f $ Set.toList $ getNonTerminals grammar
-      where 
-        f nt = (nt, Set.empty)
+firstSetInit :: Grammar -> FirstSetMap
+firstSetInit grammar = Map.fromList 
+                     $ fmap f 
+                     $ Set.toList 
+                     $ getNonTerminals grammar
+  where 
+    f = \x -> (x, Set.empty)
 
-    -- Add Empty string for First set of every symbols
-    firstEmptyString :: Grammar -> FirstSetMap
+-- Add Empty string into First set of current production
+-- if prod :: 'E' | ...
+firstSetEmptyString :: Grammar -> FirstSetMap -> FirstSetMap
+firstSetEmptyString (rule:remain) firstSet
+    | [Left Epsilon] `elem` (rhs rule) = firstSetEmptyString remain $ insertIntoFirstSet firstSet prod Epsilon 
+    | not $ [Left Epsilon] `elem` (rhs rule) = firstSetEmptyString remain firstSet
+  where 
+    prod = lhs rule
 
-    -- Add terminals if it's first symbol in rhs 
-    firstTerminal :: Grammar -> FirstSetMap
-
-    first :: Grammar -> FirstSetMap -> FirstSetMap
-    first (rule:grammar) firstSet = 
-      where 
-        firstRhs :: Rule -> Set Terminal
-        firstRhs Rule x (yi:y) = 
-            | yi == Right terminal = Set.singleton terminal
-            | yi == Left nonterminal = if Set.member Epsilon firstOfNt
-                                       then Set.Union termsForY rirstRhs Rule x y
-                                       else termsForY
-          where
-            firstOfNt = getFirstSet nonterminal firstSet 
-            termsForY = Set.delete Epsilon firstOfNt
-
-
-
-
-followSet :: Map k v -> Map k v -> Map k v
-followSet grammar follow = 
-
-throughGrammar followSet grammarRule = 
-    let
-        rhs = head grammarRule
-        lhs = tail grammarRule
-        followSymbol = followSymb
-    in
-
-followSymbol symbols
-    | is terminal last element = firstSet(symbol) - E
-    | not is terminal last element  || terminal last and firstSet(terminal) U E = 
-    
+-- Add terminals if it's first symbol in rhs 
+--firstSetTerminal :: Grammar -> FirstSetMap
+--firstSetTerminal grammar firstSet = 
+--
+--firstSetRhs :: Grammar -> FirstSetMap -> FirstSetMap
+--first (rule:grammar) firstSet = 
+--  where 
+--    firstRhs :: Rule -> Set Terminal
+--    firstRhs Rule x (yi:y) = 
+--        | yi == Right terminal = Set.singleton terminal
+--        | yi == Left nonterminal = if Set.member Epsilon firstOfNt
+--                                   then Set.Union termsForY rirstRhs Rule x y
+--                                   else termsForY
+--      where
+--        firstOfNt = getFirstSet nonterminal firstSet 
+--        termsForY = Set.delete Epsilon firstOfNt
+--
+--
+--
+--
+--followSet :: Map k v -> Map k v -> Map k v
+--followSet grammar follow = 
+--
+--throughGrammar followSet grammarRule = 
+--    let
+--        rhs = head grammarRule
+--        lhs = tail grammarRule
+--        followSymbol = followSymb
+--    in
+--
+--followSymbol symbols
+--    | is terminal last element = firstSet(symbol) - E
+--    | not is terminal last element  || terminal last and firstSet(terminal) U E = 
+--    
