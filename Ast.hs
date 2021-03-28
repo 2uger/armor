05a47b5d18cc3af9e98988bbd448993fddef8e2c
Thuger <oleg.ignatenko12@gmail.com>
2021-03-28 13:43:45 +0300

    Got representation of AST

--- Ast.hs
+++ Ast.hs
@@ -1,13 +1,67 @@
 module Ast where
 
--- C language consists of three main constructions
--- declaration, statement expression
+-- C-ish language consists of three main constructions
+-- declaration(variable declaration,
+--             func declaration) 
+-- statement(statement is process of somehow changing programm
+--           state)
+-- expression(something that will have evaluated value
+--            2 + 2 will have value 4
+--            a will have value of a which we init or declare before)
 
-data Type = TypeBool | TypeInt | TypeChar
+-- ****** Declaration ******
+-- Declaration describes expressions like
+-- int a = 10;
+-- char m = 'a';
+-- int func(int x) {
+--      int m = x * 45;
+--      return m;
+-- };
+data VarDeclaration = { Name :: String
+                      , Type :: Type
+                      , InitValue :: Expression }
 
-data Declaration = { DeclName :: String
-                   , DeclType :: Type
-                   , DeclValue :: Value
-                   , DeclCode :: String }
+data FuncDeclaration = { Name :: String
+                       , Type :: Type
+                       , Code :: StatementBlock}
 
-data Statement = 
+data Type = TypeBool 
+          | TypeInt 
+          | TypeChar 
+          | TypeVoid
+          | TypeArray
+          | TypeFunction Type ParamList
+
+data ParamList = ParamList String Type ParamList
+
+-- ****** Statement ******
+data Statement = VarDecl | Expression | IfElse | ForLoop | Return
+
+data ExprIfElse = ExprIfElse { Condition :: Expression,
+                               IfCode :: StatementBlock,
+                               ElseCode :: StatementBlock }
+
+data ExprForLoop = ExprForLoop { InitExpr :: Expression,
+                                 Condition :: Expression,
+                                 NextIter :: Expression,
+                                 Body :: StatementBlock }
+
+-- Statement block will contain multiple statements
+data StatementBlock a = EmptyStmt | Stmt a StatementBlock a
+
+
+-- ****** Expression ******
+data Expression = ExprName String 
+                | ExprValue Value 
+                | ExprAdd { Left :: Expression,
+                            Right :: Expression }
+                | ExprSub { Left :: Expression,
+                            Right :: Expression }
+                | ExprMul { Left :: Expression,
+                            Right :: Expression }
+                | ExprDiv { Left :: Expression,
+                            Right :: Expression }
+                | ExprAnd { Left :: Expression,
+                            Right :: Expression }
+                | ExprOr  { Left :: Expression,
+                            Right :: Expression }
