b8a4a8dc3a5686c9c3d6024038fac0fc6da226b8
Thuger <ignatenkowork12@gmail.com>
2021-04-10 16:11:52 +0300

    Rewrite lexer a little bit to parse simplest tokens, it's enough

--- Lexer.hs
+++ Lexer.hs
@@ -2,42 +2,41 @@
     Scanner for tokens from grammar.txt 
     Based on simple DFA by table driven approach
 -}
-
 module Lexer where
-
-import Data.Char
 
 import ParserTypes 
 
+import Data.Char
 
-printResults :: [Token] -> String
+printResults :: [Terminal] -> String
 printResults [] = error "Empty List!"
 printResults (x:xs)= (show x ++ " | ") ++ printResults xs
 
-removeWhiteSpace :: [Token] -> [Token]
-removeWhiteSpace [] = []
-removeWhiteSpace tokens = [token | token <- tokens, token /= TokenWhiteSpace]
 
-parseTokens :: String -> [Token]
+parseTokens :: String -> [Terminal]
 parseTokens [] = []
 parseTokens input = 
         let 
             (token, remain) = driveTable 0 "" input
         in 
-            removeWhiteSpace $ token : parseTokens remain
+            token : parseTokens remain
+  where
+    --removeWhiteSpace :: [Token] -> [Token]
+    --removeWhiteSpace [] = []
+    --removeWhiteSpace tokens = [token | token <- tokens, token /= TokenWhiteSpace]
+
 
 -- By state and next char it detect if it correct and next state
-driveTable :: Int -> String -> String -> (Token, String)
-driveTable currState tokenStr [] = (TokenInvalid tokenStr, "")
+driveTable :: Int -> String -> String -> (Terminal, String)
+driveTable currState tokenStr [] = (TermEmpty, "")
 driveTable state tokenStr (c:xs) = 
     let 
-        (state', isConsume) = nextState state c
+        (nstate, isConsume, isFinalState) = nextState state c
         (tokenStr', remain) = nextTokenString tokenStr c xs isConsume
-        (done, token) = finalState state' tokenStr' 
     in
-        if done
-            then (token, remain)
-        else driveTable state' tokenStr' remain
+        case isFinalState of
+            True -> (tokenByState nstate, remain)
+            False -> driveTable nstate tokenStr' remain
         
 
 nextTokenString :: String -> Char -> String -> Bool -> (String, String)
@@ -47,63 +46,159 @@ nextTokenString tokenStr c remain isConsume
 
 
 -- finalState define is current state is final state or not
-finalState :: Int -> String -> (Bool, Token)
-finalState x tokenStr
-        | x == 1    = (True, Term "+" )
-        | x == 2    = (True, TokenMinus tokenStr)
-        | x == 3    = (True, TokenMul )
-        | x == 4    = (True, TokenEqual tokenStr)
-        | x == 5    = (True, TokenLParen)
-        | x == 6    = (True, TokenRParen)
-        | x == 7    = (True, TokenWhiteSpace)
-        | x == 8    = (True, TokenSemicolon)
-        | x == 12   = (True, TokenTypeInt)
-        | x == 14   = (True, TokenID)
-        | x == 16   = (True, TokenNum)
-        | x == 99   = (True, TokenInvalid tokenStr)
-        | otherwise = (False, TokenNum)
+tokenByState :: Int -> Terminal
+tokenByState state 
+    | state == 1  = TermPlus          
+    | state == 2  = TermMinus
+    | state == 3  = TermMultiply
+    | state == 4  = TermDivide
+    | state == 5  = TermDivRemainder
+    | state == 6  = TermEqual
+    | state == 7  = TermLParen
+    | state == 8  = TermRParen
+    | state == 9  = TermLSqBracket
+    | state == 10  = TermRSqBracket
+    | state == 11  = TermLBrace
+    | state == 12  = TermRBrace
+    | state == 13  = TermBackQuote
+    | state == 14  = TermComma
+    | state == 15  = TermColon
+
+    | state == 16  = TermInt
+    | state == 23  = TermBool
+    | state == 26  = TermNumConst
+    | state == 34  = TermId
+    | state == 99 = TermEmpty
+   -- | state == 3  = TokenMul
+   -- | state == 4  = TokenEqual
+   -- | state == 5  = TokenLParen
+   -- | state == 6  = TokenRParen
+   -- | state == 7  = TokenWhiteSpace
+   -- | state == 8  = TokenSemicolon
+   -- | state == 12 = TokenTypeInt
+   -- | state == 14 = TokenID
+   -- | state == 16 = TokenNum
+   -- | state == 99 = TokenInvalid tokenStr)
+   -- | otherwise = (False, TokenNum)
 
 
 -- nextState consume initial state and next char
 -- :return (next state, should you consume next symbol)
-nextState :: Int -> Char -> (Int, Bool)
+nextState :: Int -> Char -> (Int, Bool, Bool)
 nextState 0 c 
-        | c == '+' = (1, True)  
-        | c == '-' = (2, True)
-        | c == '*' = (3, True)
-        | c == '=' = (4, True)
-        | c == '(' = (5, True)
-        | c == ')' = (6, True)
-        | c == ' ' = (7, True)
-        | c == ';' = (8, True)
-        | c == 'i' = (9, True)
-        | c `elem` ['a'..'z'] = (13, True)
-        | c `elem` ['1'..'9'] = (15, True)
-        | otherwise = (99, True)
+    | c == '+' = (1, True, True)  
+    | c == '-' = (2, True, True)
+    | c == '*' = (3, True, True)
+    | c == '/' = (4, True, True)
+    | c == '%' = (5, True, True)
+    | c == '=' = (6, True, True)
 
-nextState 9 c
-        | c == 'n' = (10, True)
-        | c `elem` ['a'..'z'] || c `elem` ['1'..'9'] = (13, True)
-        | otherwise = (99, True)
+    | c == '(' = (7, True, True)
+    | c == ')' = (8, True, True)
+    | c == '[' = (9, True, True)
+    | c == ']' = (10, True, True)
+    | c == '{' = (11, True, True)
+    | c == '}' = (12, True, True)
 
-nextState 10 c
-        | c == 't' = (11, True)
-        | c `elem` ['a'..'z'] || c `elem` ['1'..'9'] = (13, True)
-        | otherwise = (99, True)
+    | c == ';' = (13, True, True)
+    | c == ',' = (14, True, True)
+    | c == ':' = (15, True, True)
+    | c == ' ' = (0, True, False)
+    
+    -- Start state for TermInt
+    | c == 'i' = (14, True, False)
+    -- Start state for TermBool
+    | c == 'b' = (15, True, False)
+    -- Start state for TermChar
+    | c == 'c' = (16, True, False)
 
-nextState 11 c
-        | c == ' ' = (12, False)
-        | c `elem` ['a'..'z'] || c `elem` ['1'..'9'] = (13, True)
-        | otherwise = (99, True)
+   -- -- Start state for TermStatic
+   -- | c == 's' = (17, True)
+   -- -- Start state for TermWhile
+   -- | c == 's' = (18, True)
+   -- -- Start state for TermReturn
+   -- | c == 's' = (19, True)
+   -- -- Start state for TermBreak
+   -- | c == 's' = (20, True)
 
-nextState 13 c
-        | c `elem` ['a'..'z'] || c `elem` ['1'..'9'] = (13, True)
-        | c == ' ' = (14, False)
-        | otherwise = (99, True) 
+   -- -- Start state for TermNumConst
+    | c `elem` ['1'..'9'] = (26, True, False)
+   -- -- Start state for TermStringConst
+   -- | c `elem` ['a'..'z'] = (22, True)
+   -- -- Start state for TermCharConst
+   -- | c == '\'' = (23, True)
+
+   -- -- Start state for TermId
+   -- | c `elem` ['a'..'z'] = (24, True)
+   -- | otherwise = (99, True)
+
+nextState 14 c
+    | c == 'n' = (15, True, False)
+    | c `elem` ['a'..'z'] = (34, True, False)
 
 nextState 15 c
-        | c `elem` ['1'..'9'] = (15, True)
-        | c `elem` ['a'..'z'] = (13, True)
-        | c `elem` ['(', ')', '*', '=', ';', '+', '-'] = (16, False)
-        | c == ' ' = (16, False)
-        | otherwise = (15, False)
+    | c == 't' = (16, True, False)
+    
+nextState 16 c
+    | c == ' ' = (16, True, True)
+    | c `elem` ['a'..'z'] = (34, True, False)
+    | otherwise = error "Unexpected symbol after type declaration"
+
+-- For bool type
+nextState 17 c
+    | c == 'o' = (18, True, False)
+    | otherwise = (34, True, False)
+
+nextState 18 c
+    | c == 'o' = (19, True, False)
+    | otherwise = (34, True, False)
+
+nextState 19 c
+    | c == 'l' = (18, True, False)
+    | otherwise = (34, True, False)
+
+nextState 20 c
+    | c == ' ' = (20, True, True)
+    | c `elem` ['a'..'z'] = (34, True, False)
+    | otherwise = error "Unexpected symbol after type declaration"
+
+-- For char type
+-- nextState 21-25
+-- For NumConst
+nextState 26 c
+    | c `elem` ['0'..'9'] = (26, True, False)
+    | c `elem` [']', ';'] = (26, False, True)
+    | c == ' ' = (26, True, True)
+    | otherwise = error "Unexpected symbol after numbers"
+
+nextState 34 c
+    | c `elem` ['a' .. 'z'] = (34, True, False)
+    | c `elem` ['0' .. '9'] = (34, True, False)
+    | otherwise = (34, True, True)
+--nextState 9 c
+--        | c == 'n' = (10, True)
+--        | c `elem` ['a'..'z'] || c `elem` ['1'..'9'] = (13, True)
+--        | otherwise = (99, True)
+--
+--nextState 10 c
+--        | c == 't' = (11, True)
+--        | c `elem` ['a'..'z'] || c `elem` ['1'..'9'] = (13, True)
+--        | otherwise = (99, True)
+--
+--nextState 11 c
+--        | c == ' ' = (12, False)
+--        | c `elem` ['a'..'z'] || c `elem` ['1'..'9'] = (13, True)
+--        | otherwise = (99, True)
+--
+--nextState 13 c
+--        | c `elem` ['a'..'z'] || c `elem` ['1'..'9'] = (13, True)
+--        | c == '_' = (13, True)
+--        | c == ' ' = (14, False)
+--        | otherwise = (99, True) 
+--
+--nextState 15 c
+--        | c `elem` ['1'..'9'] = (15, True)
+--        | c `elem` ['a'..'z'] = (13, True)
+--        | c `elem` ['(', ')', '*', '=', ';', '+', '-'] = (16, False)
+--        | c == ' ' = (16, False)
+--        | otherwise = (15, False)
